f658afbe4c79cc8f94b6cb62042b8f1db4b584a3
Siphiwe Manda <siphiwe.manda@bepsystems.co.uk>
2020-12-10 15:02:13 +0000

    evolving a living cell - scenarios 1 through 3

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,65 @@
+﻿namespace GameOfLife
+{
+    public class Game
+    {
+        private readonly bool[,] _board;
+
+        public Game(bool[,] board)
+        {
+            _board = board;
+           
+        }
+
+        public void Evolve()
+        {
+
+            var newBoard = new bool[_board.GetLength(0), _board.GetLength(1)];
+
+            for (var x = 0; x < _board.GetLength(0); x++)
+            {
+                for (var y = 0; y < _board.GetLength(1); y++)
+                {
+                    GetLivingNeighborsCount(x,y);
+                }
+            }
+            _board[1, 1] = false;
+
+        }
+
+        private int GetLivingNeighborsCount(int x, int y)
+        {
+            
+            var cellOne = x - 1;
+            var totalLiveCells = 0;
+            totalLiveCells += IsAlive(x - 1, y - 1) ? 1 : 0;
+            totalLiveCells += IsAlive(x - 1, y - 0) ? 1 : 0;
+            totalLiveCells += IsAlive(x - 1, y + 1) ? 1 : 0;
+            totalLiveCells += IsAlive(x - 0, y - 1) ? 1 : 0;
+            totalLiveCells += IsAlive(x - 0, y + 1) ? 1 : 0;
+            totalLiveCells += IsAlive(x + 1, y - 1) ? 1 : 0;
+            totalLiveCells += IsAlive(x + 1, y - 0) ? 1 : 0;
+            totalLiveCells += IsAlive(x + 1, y + 1) ? 1 : 0;
+
+            return totalLiveCells;
+
+
+        }
+
+        public bool IsAlive(int x, int y)
+        {
+            var maxX = _board.GetLength(0);
+            var minX = 0;
+
+            var maxY = _board.GetLength(1);
+            var minY = 0;
+            if (x >=minX && x < maxX && y >= minY && y < maxY)
+            {
+                return _board[x, y];
+            }
+
+            return false;
+
+
+        }
+    }
+}
---
+++ GameOfLifeTests/GameTests.cs
@@ -0,0 +1,44 @@
+﻿using GameOfLife;
+using NUnit.Framework;
+
+namespace GameOfLifeTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void LivingCellWithZeroNeighborsDies()
+        {
+           
+           var board = new [,] {{false, false, false}, {false, true, false}, {false, false, false}};
+           var game = new Game(board);
+           game.Evolve();
+           Assert.That(game.IsAlive(1,1), Is.False);
+
+        }
+        [Test]
+        public void LivingCellWithOneNeighborsDies()
+        {
+
+            var board = new[,] { { false, true, false }, 
+                                 { false, true, false }, 
+                                 { false, false, false } };
+            var game = new Game(board);
+            game.Evolve();
+            Assert.That(game.IsAlive(1, 1), Is.False);
+
+        }
+        [Test]
+        public void LivingCellWithTwoNeighborsLives()
+        {
+
+            var board = new[,] { { false, true, false },
+                                { false, true, true },
+                                { false, false, false } };
+            var game = new Game(board);
+            game.Evolve();
+            Assert.That(game.IsAlive(1, 1), Is.True);
+
+        }
+    }
+}
